4ba4ce62b75c1d755e829a5540228239829d3f41
oscar066 <oscarkaruga1@gmail.com>
2024-11-11 17:02:24 +0300

    :construction: Chatpdf feature page

---
+++ karen-read-chatpdf/src/app/chatpdf/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+import ChatPDFDashboard from '../components/Chat/chat'
+
+const BlogPage: React.FC = () => {
+    return (
+      <div className="pt-8"> 
+        <ChatPDFDashboard />
+      </div>
+    );
+  };
+  
+  export default BlogPage;
