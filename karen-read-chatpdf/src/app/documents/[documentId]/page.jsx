0f0272da6c41a0aefdfb93d2df1be7ad4f380336
oscar066 <oscarkaruga1@gmail.com>
2025-02-05 16:47:39 +0300

    :construction: Add paths for dashboard and documents

---
+++ karen-read-chatpdf/src/app/documents/[documentId]/page.jsx
@@ -0,0 +1,14 @@
+// // src/app/documents/[documentId]/page.jsx
+// 'use client';
+
+// import dynamic from "next/dynamic";
+
+// // Ensure this page is client rendered.
+// const DocumentReader = dynamic(
+//     () => import("../../../components/TTS/DocumentReader"),
+//     { ssr: false }
+//   );
+
+// export default function DocumentPage() {
+//   return <DocumentReader />;
+// }
