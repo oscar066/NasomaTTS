288fb74316ff69191ad538b2791accebe61d45ba
oscar066 <oscarkaruga1@gmail.com>
2025-02-06 15:19:20 +0300

    :construction: Add apollo server for graphql

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,5 +1,6 @@
 // server.js
 const express = require('express');
+const { ApolloServer ,gql } = require('apollo-server-express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
