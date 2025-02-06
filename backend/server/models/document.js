1137c4358665cc9ed9136308402d0a8d2d202ea3
oscar066 <oscarkaruga1@gmail.com>
2025-02-06 16:12:47 +0300

    :construction: Add the user and documents schema for the db

---
+++ backend/server/models/document.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const documentSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        author: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Document = mongoose.model('Document', documentSchema);
+
+module.exports = Document;
---
+++ backend/server/models/index.js
@@ -0,0 +1,9 @@
+const Document = require('./document');
+const User = require('./user');
+
+const models = {
+    Document,
+    User
+};
+
+module.exports = models;
