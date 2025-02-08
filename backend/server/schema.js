bd1f6b3e8481e646e19de77f916125b6d50edbce
oscar066 <oscarkaruga1@gmail.com>
2025-02-08 17:35:28 +0300

    :bug: Fix typo in the graphql schema

--- backend/server/schema.js
+++ backend/server/schema.js
@@ -1,5 +1,38 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
-module.exports = gql`
+const typeDefs = gql`
+  scalar Upload
 
+  type Document {
+    id: ID!
+    title: String!
+    author: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    avatar: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type Query {
+    documents: [Document!]
+    document(id: ID!): Document
+    users: [User!]
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    uploadPDF(file: Upload!): Document!
+    deleteDocument(id: ID!): Boolean!
+    signUp(username: String!, email: String!, password: String!): String!
+    signIn(username: String!, email: String!, password: String!): String!
+  }
 `;
+
+module.exports = typeDefs;
