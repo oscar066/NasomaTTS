f37ba8618525d285bce12d87417dfa1d5d17dbbb
oscar066 <oscarkaruga1@gmail.com>
2025-02-09 16:33:23 +0300

    :construction: add GraphQL queries

---
+++ backend/server/resolvers/query.js
@@ -0,0 +1,22 @@
+const Document = require("../models/document");
+const User = require("../models/user");
+
+const Query = {
+  documents: async () => {
+    return await Document.find();
+  },
+
+  document: async (parent, { id }) => {
+    return await Document.findById(id);
+  },
+
+  users: async () => {
+    return await User.find();
+  },
+
+  user: async (parent, { id }) => {
+    return await user.findById(id);
+  },
+};
+
+module.exports = Query;
