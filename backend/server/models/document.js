929ee03b3ce85e47755f19ffc0217a77b94f9555
oscar066 <oscarkaruga1@gmail.com>
2025-02-19 16:37:58 +0300

    :construction: toJSON transform of id fields

--- backend/server/models/document.js
+++ backend/server/models/document.js
@@ -1,25 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const documentSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true
-        },
-        content: {
-            type: String,
-            required: true
-        },
-        author: {
-            type: String,
-            required: true
-        }
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
 );
 
-const Document = mongoose.model('Document', documentSchema);
+const Document = mongoose.model("Document", documentSchema);
 
 module.exports = Document;
--- backend/server/models/user.js
+++ backend/server/models/user.js
@@ -1,30 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            index: { unique: true }
-        },
-        email: {
-            type: String,
-            required: true,
-            index: { unique: true }
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        avatar: {
-            type: String
-        }
+  {
+    username: {
+      type: String,
+      required: true,
+      index: { unique: true },
     },
-    {
-        // Assigns createdAt and updatedAt fields with a Date type
-        timestamps: true
-    }
+    email: {
+      type: String,
+      required: true,
+      index: { unique: true },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+  },
+  {
+    // Assigns createdAt and updatedAt fields with a Date type
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
 );
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
