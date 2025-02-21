4fa11aba348c677cbcc21037ec1da41a7ad3e277
oscar066 <oscarkaruga1@gmail.com>
2025-02-22 00:34:12 +0300

    :construction: start with the landing page on the home or root

--- karen-read-chatpdf/src/app/page.tsx
+++ karen-read-chatpdf/src/app/page.tsx
@@ -4,12 +4,14 @@ import React from 'react';
 
 // import Dashboard from "./components/Dashboard/Dashboard";
 //import DocumentReader from "./components/TTS/DocumentReader";
-import DocumentReader from "./components/TTS/documentReader2";
+// import DocumentReader from "./components/TTS/documentReader2";
+
+import Home from "./components/Home/Home";
 
 const HomePage: React.FC = () => {
   return (
     <div className='pt-8'> 
-      <DocumentReader />
+      <Home />
     </div>
   );
 };
