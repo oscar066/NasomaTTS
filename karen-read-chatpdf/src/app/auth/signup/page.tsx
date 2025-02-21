181c939edd26b0feef19abbcb5592be51e59ffb1
oscar066 <oscarkaruga1@gmail.com>
2025-02-21 20:03:30 +0300

    :construction: add the signup page

---
+++ karen-read-chatpdf/src/app/auth/signup/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import SignupPage from "../../components/Signup/signup";
+
+const Signup: React.FC = () => {
+  return (
+    <div className="flex min-h-screen bg-white">
+      <SignupPage />
+    </div>
+  );
+};
+
+export default Signup;
