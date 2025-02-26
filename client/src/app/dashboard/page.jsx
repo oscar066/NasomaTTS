d5d75be3008b70985c0362032fdcecdcb8bce318
oscar066 <oscarkaruga1@gmail.com>
2025-02-26 16:50:18 +0300

    :construction: dashboard component loading data from db

--- client/src/app/dashboard/page.jsx
+++ client/src/app/dashboard/page.jsx
@@ -1,13 +1,10 @@
-// import TextToSpeechConverter from "../components/dashboard/TextToSpeechConverter";
-// import RecentConversions from "../components/dashboard/RecentConversions";
-// import Dashboard from "../components/Dashboard/Dashboard";
+import Dashboard from "../components/Dashboard/Dashboard";
 import React from "react";
-import DocumentReader from "../components/TTS/documentReader2";
 
 const DashBoard = () => {
   return (
     <div className='pt-8'> 
-      <DocumentReader />
+      <Dashboard />
     </div>
   );
 };
