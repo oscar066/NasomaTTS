aa12e55526964cc232b42f255464dd8234576ad9
oscar066 <oscarkaruga1@gmail.com>
2025-03-15 15:10:31 +0300

    :construction: add the modularized document Reader

--- client/src/app/documents/[documentId]/page.jsx
+++ client/src/app/documents/[documentId]/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
-import DocumentReader from "../../components/TTS/documentReader3";
+// import DocumentReader from "../../components/TTS/documentReader3";
+import DocumentReader from "../../components/DocumentReader/DocumentReader";
 
 export default function DocumentPage() {
     return <DocumentReader />;
