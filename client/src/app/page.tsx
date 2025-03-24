f80a47dfe9f0a74787ebf9d0f7aa2ccff1487ec8
oscar066 <oscarkaruga1@gmail.com>
2025-03-24 17:02:05 +0300

    :lipstick: remove the padding top on the app for more cleaner navbar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,16 +1,11 @@
 'use client';
 
 import React from 'react';
-
-// import Dashboard from "./components/Dashboard/Dashboard";
-//import DocumentReader from "./components/TTS/DocumentReader";
-// import DocumentReader from "./components/TTS/documentReader2";
-
 import Home from "./components/Home/Home";
 
 const HomePage: React.FC = () => {
   return (
-    <div className='pt-8'> 
+    <div className='pt-0'> 
       <Home />
     </div>
   );
