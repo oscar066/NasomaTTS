5799ab5823cf8e8cb6849015b320b9beb5105e24
oscar066 <oscarkaruga1@gmail.com>
2025-04-01 15:47:28 +0300

    :construction: pass all requests to graphql with an authorization header

--- client/src/lib/apolloClient.ts
+++ client/src/lib/apolloClient.ts
@@ -1,13 +1,26 @@
 // lib/apolloClient.ts
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { getSession } from 'next-auth/react';
 
 const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://localhost:5000/graphql',
   // credentials: 'include', // Include credentials if your server requires them
 });
 
+const authLink = setContext(async (_, { headers }) => {
+  const session = await getSession();
+  const authHeader = session?.accessToken ? `Bearer ${session.accessToken}` : "";
+  return {
+    headers: {
+      ...headers,
+      authorization: authHeader,
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
