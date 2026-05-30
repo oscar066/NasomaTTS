3c5e47d6a3d119e1dcc1e530c588cbf8025c900d
Oscar066 <oscarkaruga1@gmail.com>
2026-05-30 11:14:09 +0300

    :construction: Remove unused imports

--- client/src/app/documents/[documentId]/page.jsx
+++ client/src/app/documents/[documentId]/page.jsx
@@ -1,6 +1,5 @@
 'use client';
 
-// import DocumentReader from "../../components/TTS/documentReader3";
 import DocumentReader from "../../components/DocumentReader/DocumentReader";
 
 export default function DocumentPage() {
