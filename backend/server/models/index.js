8d680f2dadfd2f1b286f93c7ba03101e3adc231c
Oscar066 <oscarkaruga1@gmail.com>
2026-06-09 09:53:23 +0300

    :truck: Remove dead code

--- backend/server/models/index.js
+++
@@ -1,9 +0,0 @@
-const Document = require('./document');
-const User = require('./user');
-
-const models = {
-    Document,
-    User
-};
-
-module.exports = models;
--- backend/server/resolvers/index.js
+++
@@ -1,8 +0,0 @@
-const Query = require("./query");
-const Mutation = require("./mutation");
-
-module.exports = {
-  // Upload: GraphQLUpload,
-  Query,
-  Mutation,
-};
